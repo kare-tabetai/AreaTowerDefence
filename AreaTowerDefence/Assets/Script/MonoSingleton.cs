3ef4b795abf39cb167a21e07c312206f83783af2
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-11-24 22:33:41 +0900

    MonoSingleton更新

--- AreaTowerDefence/Assets/Script/MonoSingleton.cs
+++ AreaTowerDefence/Assets/Script/MonoSingleton.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine;
 
-public abstract class MonoSingleton<T> : MonoBehaviour
-    where T : MonoSingleton<T>
+/// <summary>
+/// 継承したクラスのStartでDontDestroyonLoad(gameObject)
+/// をするとゲーム全体でのシングルトンができる．
+/// しないとシーン内でのシングルトンができる．
+/// 継承先ではAwake,OnDestroyは使えないので，
+/// SubAwake,SubOnDestroyを使う.
+/// public class 継承クラス:MonoSingleton<継承クラス>って感じで使う.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class MonoSingleton<T> : 
+    MonoBehaviour where T : MonoSingleton<T>
 {
 
-    void Awake()
+    /// <summary>
+    /// すでにインスタンスがあれば破棄
+    /// 値をシーンごとにコピーする場合はこれをオーバーライド
+    /// </summary>
+    protected virtual void Awake()
     {
         if (_Instance != null)
         {
-            Debug.LogError(typeof(T) + "was instantiated");
+            print(typeof(T) + "was instantiated");
+            Destroy(gameObject);
+            return;
         }
         Instance = (T)this;
         SubAwake();
@@ -16,12 +31,16 @@ public abstract class MonoSingleton<T> : MonoBehaviour
 
     protected virtual void SubAwake()
     {
+
     }
 
-    void OnDestroy()
+    protected virtual void OnDestroy()
     {
         SubOnDestroy();
-        Instance = null;
+        if (Instance == this)
+        {
+            Instance = null;
+        }
     }
 
     protected virtual void SubOnDestroy()
@@ -38,7 +57,7 @@ public abstract class MonoSingleton<T> : MonoBehaviour
             }
             return _Instance;
         }
-        private set
+        protected set
         {
             _Instance = value;
         }
