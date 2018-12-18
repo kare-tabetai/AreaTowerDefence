379afddd2a5648c318ce032e754aba3f84a53ca8
kare-tabetai <yahoo.zip.lzh.exe@gmail.com>
2018-12-18 11:53:42 +0900

    アセット追加

---
+++ AreaTowerDefence/Assets/Script/ImageEffect/CameraEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffect : CustomImageEffect
+{
+    public override string ShaderName
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+    protected override void UpdateMaterial()
+    {
+
+    }
+}
